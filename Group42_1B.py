672ac4b4a93dac36e825e240cbe56c4c519b5978
Alice Lee <alice@example.com>
2025-08-05 16:15:40 +1000

    add try-except

--- Group42_1B.py
+++ Group42_1B.py
@@ -15,17 +15,21 @@ Editor: Alice Lee
 # Title for output, \n is just to space it cleanly
 print("\n-- Hollow Square Drawer --\n")
 
+#Add try-except block to make safe programming
+try:
 # Ask user for square size. Using int because we want whole numbers only.
-size = int(input("Enter the size of the square: "))
+    size = int(input("Enter the size of the square: "))
 
 # Check that the number is positive
-if size > 0:
-    for i in range(size):
+    if size > 0:
+        for i in range(size):
         # Print full row of stars for top and bottom
-        if i == 0 or i == size - 1:
-            print("* " * size)
-        else:
+            if i == 0 or i == size - 1:
+                print("* " * size)
+            else:
             # Middle rows: one star, space, then one star
-            print("*" + "  " * (size - 2) + " *")
-else:
-    print("Please enter a number greater than 0.")
+                print("*" + "  " * (size - 2) + " *")
+    else:
+        print("Please enter a number greater than 0.")
+except Exception as e:
+    print(e)
